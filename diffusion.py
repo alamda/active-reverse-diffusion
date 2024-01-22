3e7c13b42b22e25dd5d3b3e0eb4fe9bfa9756a15
alamda <alexandral@uchicago.edu>
2024-01-22 16:41:54 -0600

    barebones layout for Diffusion class

---
+++ diffusion.py
@@ -0,0 +1,24 @@
+class Diffusion:
+  def __init__(self):
+    print("placeholder init function")
+    pass
+
+  def __delete__(self):
+    pass
+
+  def forward_diffuse(self, matrix):
+    pass
+    return matrix
+
+  def backward_diffuse(self, matrix):
+    pass
+    return matrix
+
+  def calc_KL_div(self, dsn1, dsn2):
+    pass
+    return KL_div
+
+if __name__=="__main__":
+  myDiff = Diffusion()
+
+  del myDiff
