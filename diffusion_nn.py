ef1ee7d87e589fbcee70c2a38edce635ae05c6f5
alamda <alexandral@uchicago.edu>
2024-01-23 17:24:41 -0600

    added abstract class for NN based diffusion model

---
+++ diffusion_nn.py
@@ -0,0 +1,26 @@
+from abc import abstractmethod
+from diffusion import DiffusionAbstract
+
+
+class DiffusionNN(DiffusionAbstract):
+    """Abstract class for diffusion with non-analytic target (starting) dsn"""
+
+    @abstractmethod
+    def target_data(self):
+        """Input for target dsn/data"""
+
+    @abstractmethod
+    def nn_architecture(self):
+        """Definition of the NN architecture"""
+
+    @abstractmethod
+    def loss_fn(self):
+        """Definition of loss fn used by NN"""
+
+    @abstractmethod
+    def train_nn(self):
+        """Train NN on starting data"""
+
+    @abstractmethod
+    def run_nn(self):
+        """Generate target data using trained NN"""
