2dbbe4a1c0d43136a7b2f4c376c980ecd8f2b2d4
alamda <alexandral@uchicago.edu>
2024-01-23 16:26:22 -0600

    appeased the linter

--- diffusion.py
+++ diffusion.py
@@ -4,23 +4,13 @@ from abc import ABC, abstractmethod
 class DiffusionAbstract(ABC):
     """Abstract class for diffusion"""
 
-    @asbtractmethod
-    def __init__(self):
-        pass
-
-    @abstractmethod
-    def __delete__(self):
-        pass
-
     @abstractmethod
     def generate_target_dsn(self):
         """Arbitrary starting distribution"""
-        pass
 
     @abstractmethod
     def diffuse_forward(self):
         """Arbitrary forward diffusion process"""
-        pass
 
     @abstractmethod
     def calc_score(self):
@@ -30,9 +20,7 @@ class DiffusionAbstract(ABC):
             * Learned by a NN
                 * Loss function
         """
-        pass
 
     @abstractmethod
     def diffuse_backward(self):
         """Uses score from self.calc_score()"""
-        pass
