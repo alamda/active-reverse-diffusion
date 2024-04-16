868cd475f06a84aca843c9d5a0bd18dc74cf3c4d
alamda <alexandral@uchicago.edu>
2024-04-16 15:11:26 -0500

    example of memory mapping file from data into a torch tensor

---
+++ misc/sample_mmap_torch.py
@@ -0,0 +1,78 @@
+import numpy as np
+import mmap
+import struct
+import tracemalloc
+import os
+import torch
+
+test_fname = "test_data.tmp"
+
+# Shape of whole array to be written to file
+arr_shape = (100000, 2)
+
+# Number of elements to load from memory
+elements_to_load = [10, 100, 500, 1000, 5000]
+
+# Write array or random numbers to file (row-by-row/line-by-line)
+with open(test_fname, 'wb') as f: 
+    for _ in range(arr_shape[0]):
+        data = np.random.rand(arr_shape[1]).astype(np.double)
+        f.write(data.tobytes())
+   
+# Open file again to check that the original values were successfully changed to 0s 
+with open(test_fname, 'r+b') as f:
+    mm = mmap.mmap(f.fileno(), 0)
+    
+    mm_arr = np.frombuffer(mm, dtype=np.double)
+    mm_arr = torch.from_numpy(np.reshape(mm_arr, arr_shape))
+    
+    print(mm_arr[:10])   
+
+# Test max memory usage when loading different numbers of elements from disk
+for num in elements_to_load:
+    
+    # Start memory usage monitoring
+    tracemalloc.start()  
+
+    with open(test_fname, 'r+b') as f:
+        # Create memory map
+        # Array not yet loaded to memory
+        mm = mmap.mmap(f.fileno(), 0)
+        
+        # Create memory-mapped array from buffer
+        # Array not yet loaded to memory
+        mm_arr = np.frombuffer(mm, dtype=np.double)
+        
+        mm_tensor = torch.from_numpy(np.reshape(mm_arr, arr_shape))
+        
+        # Load some number of elements from buffer
+        # Array not loaded to memory, only memory mapped
+        new_tensor = mm_tensor[:num]
+        
+        # Perform some calculation on subset array
+        # Subset of array loaded to memory
+        new_tensor.mean()
+
+        # tracemalloc.get_traced_memory(): (current usage, peak usage) in memory blocks
+        print(num, tracemalloc.get_traced_memory())
+        
+        # Change values in loaded portion of the array to 0s
+        mm_tensor[:num] = torch.from_numpy(np.zeros(new_tensor.shape))
+       
+        # Write updated buffer information to disk/file
+        mm.flush()
+    
+    # End memory usage monitoring
+    tracemalloc.stop()
+    
+# Open file again to check that the original values were successfully changed to 0s 
+with open(test_fname, 'r+b') as f:
+    mm = mmap.mmap(f.fileno(), 0)
+    
+    mm_arr = np.frombuffer(mm, dtype=np.double)
+    mm_arr = torch.from_numpy(np.reshape(mm_arr, arr_shape))
+    
+    print(mm_arr[:10])
+
+os.remove(test_fname)
+    
