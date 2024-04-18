9c917133e85bb4e4e5f4feec3e181217a929a9f3
alexandra lamtyugina <alexandral@uchicago.edu>
2024-04-18 12:53:20 -0500

    added scripts for plotting target sample and probability dsn from saved intermediate file

---
+++ scripts/plot_target_prob.py
@@ -0,0 +1,26 @@
+import sys
+
+sys.path.insert(0, '/home/alexandral/git_repos/active-reverse-diffusion/src/')
+
+import pickle
+import numpy as np
+from matplotlib import figure
+
+with open("data.pkl", "rb") as f:
+    data_obj = pickle.load(f)
+
+idx_arr = data_obj.target.target_hist_idx_arr
+prob_arr = data_obj.target.target_hist_idx_prob_arr
+
+num_bins_x = np.unique(idx_arr[:,0]).shape[0]
+num_bins_y = np.unique(idx_arr[:,1]).shape[0]
+
+fig = figure.Figure()
+ax = fig.subplots(1)
+
+im = ax.imshow(prob_arr.reshape(num_bins_x, num_bins_y))
+fig.colorbar(im)
+
+fig.savefig("prob.png")
+
+print(prob_arr.sum())
